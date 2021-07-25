9bf674ca36773bf10f7cf299b320986fa953b53c
tiannian <dtiannian@aliyun.com>
2021-07-25 22:43:21 +0800

    add some unuse doc

--- abcf/src/module/module.rs
+++ abcf/src/module/module.rs
@@ -2,25 +2,33 @@ use alloc::string::String;
 
 use super::{Application, Event, RPCs, Storages};
 
+/// Module.
 pub trait Module {
     // type Storages: Storages;
 
     // type Events: Events;
 
+    /// This module provided RPCs.
     type RPCs: RPCs;
 
+    /// This module provided Application.
     type Application: Application;
 
+    /// Get module metadata.
     fn metadata(&self) -> ModuleMetadata;
 
+    /// Return application instance.
     fn application(&self) -> Self::Application;
-    // fn events(&self) -> Self::Events;
+
+    /// Return rpcs instance.
     fn rpcs(&self) -> Self::RPCs;
-    // fn storages(&self) -> Self::Storages;
 }
 
 pub struct ModuleMetadata {
+    /// Name of module.
     pub name: String,
+    /// Version of module. If this version change, means module need update.
     pub version: String,
+    /// Version of impl. If this version change, means module only a change of impl.
     pub impl_version: String,
 }
