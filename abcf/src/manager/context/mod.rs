aaaac09b0d4e35d4d48ea4b3b00ad539a08ca1a1
tiannian <dtiannian@aliyun.com>
2021-09-06 01:31:35 +0800

    remove storage context for new manager

--- abcf/src/manager/context/mod.rs
+++ abcf/src/manager/context/mod.rs
@@ -1,14 +1,10 @@
 mod events;
 pub use events::{EventContext, EventContextImpl};
 
-pub mod storage;
-pub use storage::StorageContext;
-
 mod call;
 pub use call::CallContext;
 
 pub struct Context<'a> {
     pub event: Option<EventContext<'a>>,
-    pub storage: StorageContext,
     pub calls: CallContext<'a>,
 }
