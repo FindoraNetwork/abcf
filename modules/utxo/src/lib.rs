2170692f38353ab9950abe27d8f88238a8c62ae7
tiannian <dtiannian@gmail.com>
2021-09-08 23:23:11 +0800

    change utxo

--- modules/utxo/src/lib.rs
+++ modules/utxo/src/lib.rs
@@ -4,7 +4,7 @@
 pub struct Event1 {}
 
 #[abcf::module(name = "mock", version = 1, impl_version = "0.1.1", target_height = 0)]
-pub struct MockModule<S, D>
+pub struct UTXOModule<S, D>
 where
     S: abcf::bs3::Store,
     D: digest::Digest,
@@ -14,15 +14,15 @@ where
     pub maker_s: PhantomData<S>,
     pub maker_d: PhantomData<D>,
     #[stateful]
-    pub sf_value: BValue<u32>,
+    pub sf_value: Value<u32>,
     #[stateless]
-    pub sl_value: BValue<u32>,
+    pub sl_value: Value<u32>,
     #[stateless]
     pub sl_map: Map<i32, u32>,
 }
 
-#[abcf::rpcs(module = "mock")]
-impl<S, D> MockModule<S, D> 
+#[abcf::rpcs(module = "utxo")]
+impl<S, D>UTXOModule<S, D> 
 where
     S: abcf::bs3::Store,
     D: digest::Digest,
@@ -31,7 +31,7 @@ where
 
 /// Module's block logic.
 #[abcf::application]
-impl<S, D> Application<abcf::Stateless<Self>, abcf::Stateful<Self>> for MockModule<S, D>
+impl<S, D> Application<abcf::Stateless<Self>, abcf::Stateful<Self>> for UTXOModule<S, D>
 where
     S: abcf::bs3::Store + 'static,
     D: digest::Digest + Send + Sync,
@@ -39,7 +39,7 @@ where
 }
 
 /// Module's methods.
-impl<S, D> MockModule<S, D>
+impl<S, D> UTXOModule<S, D>
 where
     S: abcf::bs3::Store,
     D: digest::Digest,
