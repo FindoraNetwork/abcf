481740f5786574718ef3f82eba3cf63221876e48
tiannian <dtiannian@gmail.com>
2021-09-09 02:59:14 +0800

    fix error in utxo

--- modules/utxo/src/lib.rs
+++ modules/utxo/src/lib.rs
@@ -2,19 +2,15 @@
 
 use serde::{Serialize, Deserialize};
 use abcf::{Event, Application};
-use std::marker::PhantomData;
 use bs3::model::{Value, Map};
 
 /// Module's Event
 #[derive(Clone, Debug, Deserialize, Serialize, Event)]
 pub struct Event1 {}
 
-#[abcf::module(name = "mock", version = 1, impl_version = "0.1.1", target_height = 0)]
+#[abcf::module(name = "utxo", version = 1, impl_version = "0.1.1", target_height = 0)]
 pub struct UTXOModule {
-    // /// In memory.
     pub inner: u32,
-    pub maker_s: PhantomData<S>,
-    pub maker_d: PhantomData<D>,
     #[stateful]
     pub sf_value: Value<u32>,
     #[stateless]
