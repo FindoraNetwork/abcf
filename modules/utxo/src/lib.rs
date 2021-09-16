4a6850afb96eb37289282d59de7fcb940f0d196d
malefooo <31642966+malefooo@users.noreply.github.com>
2021-09-16 23:45:31 +0800

    modify module macro (#46)
    * modify module macro
    
    * fix module macro bug
    and modify manager macro check_tx to multi

--- modules/utxo/src/lib.rs
+++ modules/utxo/src/lib.rs
@@ -5,32 +5,38 @@ use std::marker::PhantomData;
 use abcf::{
     abci::{RequestBeginBlock, RequestEndBlock},
     manager::{AContext, TContext},
-    module::types::{
-        RequestCheckTx, RequestDeliverTx, ResponseCheckTx, ResponseDeliverTx, ResponseEndBlock,
+    module::{
+        types::{
+            RequestCheckTx, RequestDeliverTx, ResponseCheckTx, ResponseDeliverTx, ResponseEndBlock,
+        },
+        StorageTransaction,
     },
     Application, Event, Result, Stateful, StatefulBatch, Stateless, StatelessBatch,
 };
 use bs3::model::{Map, Value};
 use serde::{Deserialize, Serialize};
+use std::fmt::Debug;
 
 /// Module's Event
 #[derive(Clone, Debug, Deserialize, Serialize, Event)]
 pub struct Event1 {}
 
-pub trait Config {}
+pub trait Config: Send + Sync {
+    type Ty: Debug + Clone + Serialize + for<'de> Deserialize<'de> + Send + Sync + 'static;
+}
 
 #[abcf::module(name = "utxo", version = 1, impl_version = "0.1.1", target_height = 0)]
 pub struct UTXOModule<C: Config> {
     pub inner: u32,
     marker: PhantomData<C>,
     #[stateful]
-    pub sf_value: Value<u32>,
+    pub sf_value: Value<C::Ty>,
     #[stateful]
-    pub sf_value1: Value<u32>,
+    pub sf_value1: Value<C::Ty>,
     #[stateless]
-    pub sl_value: Value<u32>,
+    pub sl_value: Value<C::Ty>,
     #[stateless]
-    pub sl_map: Map<i32, u32>,
+    pub sl_map: Map<i32, C::Ty>,
 }
 
 #[abcf::rpcs]
