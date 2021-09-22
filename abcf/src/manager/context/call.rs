f7a76b97864682593ba0fd9f8432286a9cb6505d
tiannian <dtiannian@gmail.com>
2021-09-23 02:47:43 +0800

    use btreemap as call

--- abcf/src/manager/context/call.rs
+++ abcf/src/manager/context/call.rs
@@ -1,15 +1,14 @@
 use core::any::Any;
 
-use alloc::{boxed::Box, string::String, vec::Vec};
+use alloc::{boxed::Box, collections::BTreeMap, string::String, vec::Vec};
 
 pub struct CallEntry {
-    pub module: String,
     pub method: String,
     pub args: Vec<Box<dyn Any + Sync + Send>>,
 }
 
 pub struct CallContext<'a> {
-    pub entries: &'a mut Vec<CallEntry>,
+    pub entries: &'a mut BTreeMap<String, Vec<CallEntry>>,
 }
 
 impl<'a> CallContext<'a> {
@@ -18,16 +17,30 @@ impl<'a> CallContext<'a> {
             entries: &mut i.entries,
         }
     }
+
+    pub fn pop_module_calls(&mut self, name: &str) -> Option<Vec<CallEntry>> {
+        self.entries.remove(name)
+    }
+
+    pub fn push_module_call(&mut self, name: &str, call: CallEntry) {
+        if let Some(v) = self.entries.get_mut(name) {
+            v.push(call);
+        } else {
+            let mut value = Vec::new();
+            value.push(call);
+            self.entries.insert(String::from(name), value);
+        }
+    }
 }
 
 pub struct CallImpl {
-    pub entries: Vec<CallEntry>,
+    pub entries: BTreeMap<String, Vec<CallEntry>>,
 }
 
 impl CallImpl {
     pub fn new() -> Self {
         Self {
-            entries: Vec::new(),
+            entries: BTreeMap::new(),
         }
     }
 }
