3b8d7797416cdd684fb0acc169de95a6c7d47bd7
tiannian <dtiannian@gmail.com>
2021-09-28 15:10:54 +0800

    fix prefix of 0x

--- sdk/src/sender.rs
+++ sdk/src/sender.rs
@@ -4,12 +4,14 @@ use crate::{
     providers::Provider,
 };
 use abcf::module::ToBytes;
+use alloc::string::String;
 use serde_json::{json, Value};
 
 pub async fn send_tx<P: Provider, T: ToBytes>(mut p: P, method: &str, tx: &T) -> Result<Value> {
     {
-        let tx_hex = hex::encode(tx.to_bytes()?);
+        let tx_hex = String::from("0x") + &hex::encode(tx.to_bytes()?);
         let j = json!({ "tx": tx_hex });
+        log::debug!("Send object is {}", j);
         let resp: Response<Value> = p.request(&method, &j).await?;
 
         return if let Some(v) = resp.result {
