5381d27080581d43c748fbe57e63fe1f01b8c504
tiannian <dtiannian@gmail.com>
2021-09-28 15:56:23 +0800

    add serialize for string

--- sdk/src/jsonrpc/endpoint/abci_query.rs
+++ sdk/src/jsonrpc/endpoint/abci_query.rs
@@ -3,10 +3,10 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Request {
-    #[serde(serialize_with = "hex::serialize")]
+    #[serde(serialize_with = "super::serialize")]
     pub path: String,
 
-    #[serde(serialize_with = "hex::serialize")]
+    #[serde(serialize_with = "super::serialize")]
     pub data: String,
 
     pub height: Option<String>,
--- sdk/src/jsonrpc/endpoint/mod.rs
+++ sdk/src/jsonrpc/endpoint/mod.rs
@@ -2,6 +2,16 @@ pub mod abci_query;
 pub mod subscribe;
 pub mod unsubscribe;
 
-pub trait ToRequest {
-    fn to_request(&self) -> serde_json::Value;
+use alloc::string::String;
+use hex::ToHex;
+use serde::Serializer;
+
+pub fn serialize<S, T>(data: T, serializer: S) -> core::result::Result<S::Ok, S::Error>
+where
+    S: Serializer,
+    T: ToHex,
+{
+    let s = data.encode_hex::<String>();
+    let result = String::from("0x") + &s;
+    serializer.serialize_str(&result)
 }
