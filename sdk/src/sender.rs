9faf4e434a015539072b3f01e2226dd6ad9744c6
malefooo <784479660@qq.com>
2021-10-08 15:40:51 +0800

    feat query tx

--- sdk/src/sender.rs
+++ sdk/src/sender.rs
@@ -9,3 +9,10 @@ pub async fn send_tx<P: Provider, T: ToBytes>(mut p: P, method: &str, tx: &T) ->
     log::debug!("Send object is {}", j);
     p.request(&method, &j).await
 }
+
+pub async fn query_tx<P: Provider>(mut p: P, method: &str, hash: &str) -> Result<Value> {
+    let hash = String::from("0x") + hash;
+    let j = json!({"hash": hash});
+    log::debug!("Send object is {}", j);
+    p.request(&method, &j).await
+}
