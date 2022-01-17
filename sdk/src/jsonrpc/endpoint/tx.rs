133f78d7052306336439a3ecc63ff0a6fb867094
malefooo <31642966+malefooo@users.noreply.github.com>
2022-01-17 15:33:38 +0800

    Fix tx resp (#84)

--- sdk/src/jsonrpc/endpoint/tx.rs
+++ sdk/src/jsonrpc/endpoint/tx.rs
@@ -1,8 +1,6 @@
 use alloc::{string::String, vec::Vec};
 use serde::{Deserialize, Serialize};
 
-use super::event::Event;
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Request {
     #[serde(serialize_with = "super::serialize")]
@@ -28,22 +26,13 @@ pub struct Response {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ResultResponse {
-    pub code: i64,
+    pub code: String,
+
+    pub data: String,
+
+    pub log: String,
 
     pub codespace: String,
 
-    #[serde(deserialize_with = "super::deserialize_bytes")]
-    pub data: Vec<u8>,
-
-    pub events: Vec<Event>,
-
-    #[serde(deserialize_with = "super::deserialize_i64")]
-    pub gas_used: i64,
-
-    #[serde(deserialize_with = "super::deserialize_i64")]
-    pub gas_wanted: i64,
-
-    pub info: String,
-
-    pub log: String,
+    pub hash: String,
 }
